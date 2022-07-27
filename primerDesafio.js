bcfc5566c4db4bf46367b0ec203454fac2e3eac2
NicolasPerezErro <nico_perez99@hotmail.com>
2022-07-27 17:48:42 -0300

    agrego primer y segundo desafio

---
+++ primerDesafio.js
@@ -0,0 +1,44 @@
+class Usuario{
+
+    constructor(nombre,apellido,libros,mascotas){
+       this.nombre = nombre;
+       this.apellido = apellido;
+       this.libros = libros;
+       this.mascotas = mascotas;
+    }
+
+    getFullName(){
+        const nombreYApellido = `${this.nombre} ${this.apellido}`; 
+        return nombreYApellido;
+    }
+
+    addMascotas(nombreMascota){
+        this.mascotas.push(nombreMascota);
+    }
+    
+    countMascotas(){
+        return this.mascotas.length;
+    }
+
+    addBook(nombre,autor){                // En este caso el parametro es el mismo que la propiedad del objeto, entonces lo asigna directamente
+        this.libros.push({nombre,autor}); // Tambien se puede poner la propiedad: parametro -> nombre: parametroNom, autor: parametroAutor
+    }
+
+    getBookNames(){
+        const nombres = [];
+        for (let i = 0; i < this.libros.length; i++){
+              nombres.push(this.libros[i].nombre);
+        }
+   
+        return nombres;
+        
+    }
+}
+
+const usuario = new Usuario('Nicolas','Perez Erro',[{nombre: 'Harry Potter', autor: 'J. K. Rowling'}],['perro','tortuga']);
+
+console.log(`Nombre y apellido: ${usuario.getFullName()}`);
+usuario.addMascotas('gato');
+console.log(`Cantidad de mascotas: ${usuario.countMascotas()}`);
+usuario.addBook('La Isla Misteriosa','Julio Verne');
+console.log(usuario.getBookNames());
